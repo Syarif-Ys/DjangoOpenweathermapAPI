697abd547c68a830399c67380ce54aeec430dd4b
Syarif Ys <5yarif.y.s@gmail.com>
2022-09-23 21:05:12 +0800

    23 september 2022

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
   appid = 'c50e621666c9830dc5ea0bb0d3acc870'
   URL = 'https://api.openweathermap.org/data/2.5/weather?id='
-  return render(request, 'weatherapp/index.html', {'num': num})
+  PARAMS = {'q': 'amsterdam', 'appid': appid, 'units': 'metric'}
+  r = requests.get(url=URL, params=PARAMS)
+  res = r.json()
+  description = res['weather'][0]['description']
+  icon = res['weather'][0]['icon']
+  temp = res['main']['temp']
+  return render(request, 'weatherapp/index.html', {'description': description, 'icon':icon, 'temp':temp})
