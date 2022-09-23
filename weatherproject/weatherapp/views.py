6c75fdd95dc00550866c2a48f2e24afc49c74e7c
Syarif Ys <5yarif.y.s@gmail.com>
2022-09-23 21:40:22 +0800

    23 september 2022

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 import requests
+import datetime
 
 # Create your views here.
 def index(request):
+  if 'city' in request.POST:
+    city = request.POST['city']
+  else:
+    city = 'Amsterdam'
+
   appid = 'c50e621666c9830dc5ea0bb0d3acc870'
   URL = 'https://api.openweathermap.org/data/2.5/weather?id='
-  PARAMS = {'q': 'amsterdam', 'appid': appid, 'units': 'metric'}
+  PARAMS = {'q': city, 'appid': appid, 'units': 'metric'}
   r = requests.get(url=URL, params=PARAMS)
   res = r.json()
   description = res['weather'][0]['description']
   icon = res['weather'][0]['icon']
   temp = res['main']['temp']
-  return render(request, 'weatherapp/index.html', {'description': description, 'icon':icon, 'temp':temp})
+
+  day = datetime.date.today()
+
+  return render(request, 'weatherapp/index.html', {'description': description, 'icon':icon, 'temp':temp, 'day':day, 'city':city})
